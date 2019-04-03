7c9412092b4fbffac91c228afa22939eb3c0ada0
Tomas Lingotti <tomaslingotti@hotmail.com>
2019-04-03 01:56:35 -0300

    Improvement in POM

--- src/main/java/com/pya/scoreservice/model/ScoreRepository.java
+++ src/main/java/com/pya/scoreservice/model/ScoreRepository.java
@@ -1,6 +1,9 @@
 package com.pya.scoreservice.model;
 
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ScoreRepository extends MongoRepository<Score, String> {
+
+  Optional<Score> findBySaleIdentifier(String saleId);
 }
---
+++ src/main/java/com/pya/scoreservice/usecase/find/FindBySaleServiceImpl.java
@@ -0,0 +1,22 @@
+package com.pya.scoreservice.usecase.find;
+
+import com.pya.scoreservice.model.Score;
+import com.pya.scoreservice.model.ScoreRepository;
+
+public class FindBySaleServiceImpl implements FindScoreBySaleService {
+
+  private final ScoreRepository scoreRepository;
+
+  public FindBySaleServiceImpl(final ScoreRepository scoreRepository) {
+    this.scoreRepository = scoreRepository;
+  }
+
+  @Override
+  public Score findScoreBySale(final Request request) {
+    return scoreRepository
+        .findBySaleIdentifier(request.getSaleId())
+        .orElseThrow(
+            () -> new IllegalArgumentException("The score with sale id " + request.getSaleId() + " does not exists.")
+        );
+  }
+}
